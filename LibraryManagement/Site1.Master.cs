27b21b3474b4adfa8b07e8b016c79af97098c3f9
saeefahmed <saeef.ahmed@gmail.com>
2021-01-28 17:17:03 +0600

    Multi User Login Page with ASP.NET Part - 2

--- LibraryManagement/Site1.Master.cs
+++ LibraryManagement/Site1.Master.cs
@@ -11,7 +11,62 @@ namespace LibraryManagement
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                if (string.IsNullOrEmpty((string)Session["role"]))
+                {
+                    LinkButton1.Visible = true; // user login link button
+                    LinkButton2.Visible = true; // sign up link button
 
+                    LinkButton3.Visible = false; // logout link button
+                    LinkButton7.Visible = false; // hello user link button
+
+
+                    LinkButton6.Visible = true; // admin login link button
+                    LinkButton11.Visible = false; // author management link button
+                    LinkButton12.Visible = false; // publisher management link button
+                    LinkButton8.Visible = false; // book inventory link button
+                    LinkButton9.Visible = false; // book issuing link button
+                    LinkButton10.Visible = false; // member management link button
+
+                }
+                else if (Session["role"].Equals("user"))
+                {
+                    LinkButton1.Visible = false; // user login link button
+                    LinkButton2.Visible = false; // sign up link button
+
+                    LinkButton3.Visible = true; // logout link button
+                    LinkButton7.Visible = true; // hello user link button
+                    LinkButton7.Text = "Hello " + Session["username"].ToString();
+
+
+                    LinkButton6.Visible = true; // admin login link button
+                    LinkButton11.Visible = false; // author management link button
+                    LinkButton12.Visible = false; // publisher management link button
+                    LinkButton8.Visible = false; // book inventory link button
+                    LinkButton9.Visible = false; // book issuing link button
+                }
+                else if (Session["role"].Equals("admin"))
+                {
+                    LinkButton1.Visible = false; // user login link button
+                    LinkButton2.Visible = false; // sign up link button
+
+                    LinkButton3.Visible = true; // logout link button
+                    LinkButton7.Visible = true; // hello user link button
+                    LinkButton7.Text = "Hello Admin";
+
+
+                    LinkButton6.Visible = false; // admin login link button
+                    LinkButton11.Visible = true; // author management link button
+                    LinkButton12.Visible = true; // publisher management link button
+                    LinkButton8.Visible = true; // book inventory link button
+                    LinkButton9.Visible = true; // book issuing link button
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
 
         protected void LinkButton6_Click(object sender, EventArgs e)
@@ -62,7 +117,24 @@ namespace LibraryManagement
         //logout button
         protected void LinkButton3_Click(object sender, EventArgs e)
         {
+            Session["username"] = "";
+            Session["fullname"] = "";
+            Session["role"] = "";
+            Session["status"] = "";
 
+            LinkButton1.Visible = true; // user login link button
+            LinkButton2.Visible = true; // sign up link button
+
+            LinkButton3.Visible = false; // logout link button
+            LinkButton7.Visible = false; // hello user link button
+
+
+            LinkButton6.Visible = true; // admin login link button
+            LinkButton11.Visible = false; // author management link button
+            LinkButton12.Visible = false; // publisher management link button
+            LinkButton8.Visible = false; // book inventory link button
+            LinkButton9.Visible = false; // book issuing link button
+            LinkButton10.Visible = false; // member management link button
         }
 
         // view profile
@@ -71,4 +143,4 @@ namespace LibraryManagement
 
         }
     }
-}
+ }
--- LibraryManagement/userlogin.aspx.cs
+++ LibraryManagement/userlogin.aspx.cs
@@ -20,33 +20,38 @@ namespace LibraryManagement
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            try
-            {
+            try {
                 SqlConnection con = new SqlConnection(strcon);
-
-                if(con.State == ConnectionState.Closed)
+                if (con.State == ConnectionState.Closed)
                 {
                     con.Open();
+
                 }
                 SqlCommand cmd = new SqlCommand("select * from member_master_tbl where member_id='" + TextBox1.Text.Trim() + "' AND password='" + TextBox2.Text.Trim() + "'", con);
-
                 SqlDataReader dr = cmd.ExecuteReader();
                 if (dr.HasRows)
                 {
                     while (dr.Read())
                     {
-                        Response.Write("<script>alert('"+dr.GetValue(8).ToString()+"');</script>");
+                        Response.Write("<script>alert('" + dr.GetValue(8).ToString() + "');</script>");
+                        Session["username"] = dr.GetValue(8).ToString();
+                        Session["fullname"] = dr.GetValue(0).ToString();
+                        Session["role"] = "user";
+                        Session["status"] = dr.GetValue(10).ToString();
                     }
+                    Response.Redirect("homepage.aspx");
                 }
                 else
                 {
-                    Response.Write("<script>alert('Invalid Credentials');</script>");
+                    Response.Write("<script>alert('Invalid credentials');</script>");
                 }
-            }
-            catch(Exception ex)
+
+            } 
+            catch (Exception ex)
             {
 
             }
         }
+
     }
 }
